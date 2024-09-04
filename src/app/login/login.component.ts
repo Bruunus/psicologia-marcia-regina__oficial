0a612c9b3b84d4cd8970fee4a9bb8eed8d33e810
Bruno <brunus.desenvolvimentobackfront@gmail.com>
2024-09-04 17:52:23 -0300

    Pausa na implantação do login --->> login.component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, Injectable, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { RedirectComponent } from '../redirect/redirect.component';
-import { BehaviorSubject, Observable, takeLast } from 'rxjs';
+import { BehaviorSubject, interval, Observable, Subscription, takeLast } from 'rxjs';
 import { AutenticacaoService } from './autenticacao.service';
 import { Usuario } from './usuario';
 import { ApiAutenticacaoService } from '../services/autenticacao/api-autenticacao.service';
@@ -20,28 +20,81 @@ import { ApiAutenticacaoService } from '../services/autenticacao/api-autenticaca
 })
 export class LoginComponent implements OnInit {
 
-
+  // Angular services
+  formularioDeLogin!: FormGroup;
+  subscription: Subscription = new Subscription();
 
   private usuarioAutenticado: boolean = false;
+  private estadoMensagem: boolean = false;
+  private statusInputFocus: boolean = false;
+  private tentativasDeAcesso: number = 0
+
+  protected errorMessageLogin: string = '';
+  protected errorMessageSenha: string = '';
+  protected errorMessageFalhaDeAutenticacao: string = ''
+
+
+  protected loginInvalido: boolean = true;
   protected usuario: Usuario = new Usuario();
 
 
 
-  constructor(private router: Router, private autenticacaoService: AutenticacaoService,
-    private apiAutenticacaoService: ApiAutenticacaoService) {
+
+  constructor(
+    private router: Router, private autenticacaoService: AutenticacaoService,
+    private apiAutenticacaoService: ApiAutenticacaoService, private formBuilder: FormBuilder
+  ) {}
+
+
+  ngOnInit(): void {
+    this.formularioDeLogin = new FormGroup({
+      login: new FormControl(
+        this.usuario.login,
+        [
+          Validators.required,
+          Validators.maxLength(15)
+
+        ]
+
+      ),
+      senha: new FormControl(this.usuario.senha, Validators.required)
+    })
+
+    this.setupLoginForm();
+
+
+  }
+
+  onLogin() {
+
+    const loginValue = this.formularioDeLogin.get('login')!.value;
+    const senhaValue = this.formularioDeLogin.get('senha')!.value;
+
+    this.usuario.login = loginValue;
+    this.usuario.senha = senhaValue;
+
+    if(!this.validacaoLogin()) {
+      return;
+    } else {
+      this.autenticacaoService.fazerLogin(this.usuario);
+      console.log(this.usuario)
+    console.log(
+      this.login.value,' ',this.senha.value
+    )
+    }
+
+
+
+
+
 
   }
 
 
-  ngOnInit(): void {};
-
-  onLogin() {
-
-
-
-    console.log(this.usuario)
-    this.autenticacaoService.fazerLogin(this.usuario);
-
+  setupLoginForm(): void {
+    this.formularioDeLogin.get('login')?.valueChanges.subscribe((value: string) => {
+      this.formularioDeLogin.get('login')?.patchValue(value.toLowerCase(), { emitEvent: false });
+    });
   }
 
 
@@ -67,19 +120,90 @@ export class LoginComponent implements OnInit {
 
 
 
+  validacaoLogin() {
+
+     //Insira o login para acesso
+     if(this.usuario.login === '' || this.usuario.login === null) {
+      this.errorMessageLogin = 'Insira o login para acesso';
+      this.errorMessageSenha = '';
+      this.statusInputFocus = false;  // deu submit
+      return;
+     }
+
+     if(this.usuario.senha === '' || this.usuario.senha === null) {
+      this.tentativasDeAcesso++;
+
+
+
+      this.errorMessageSenha = 'Senha incorreta.';
+      this.errorMessageLogin = '';
+      this.statusInputFocus = false;  // deu submit
+
+PAUSA AQUI !
+      console.log('Tentativa: ', this.tentativasDeAcesso)
+
+      if (this.tentativasDeAcesso < 4) {
+        this.errorMessageLogin = '';
+        this.errorMessageFalhaDeAutenticacao = 'Falha de autenticação, atualize a página e tente novamente';
+        return;
+      }
+
+      return;
 
 
 
 
 
-  // getAutenticado(): Observable<boolean> {
-  //   return this.isAuthenticated.asObservable();
-  // }
+     }
 
-  // setAutenticado(value: boolean) {
-  //   this.isAuthenticated.next(value);
-  // }
+     return true;
 
 
+
+
+
+
+
+    // console.log('Validação no localStarege: Buscando dados')
+    // console.log(nomeUsuario)
+    // console.log(token)
+
+
+  }
+
+
+  focusLoginIn() {
+    this.errorMessageLogin = '';
+    this.errorMessageFalhaDeAutenticacao = '';
+  }
+
+  focusSenhaIn() {
+    this.errorMessageSenha = '';
+    this.errorMessageFalhaDeAutenticacao = '';
+  }
+
+
+
+
+
+
+
+
+  get login(): AbstractControl<string, any> {
+    return this.formularioDeLogin.get('login')!;
+  }
+
+  get senha(): AbstractControl<string, any> {
+    return this.formularioDeLogin.get('senha')!;
+  }
+
+
+  get getStatusInputFocus(): boolean {
+    return this.statusInputFocus
+  }
+
+  set setStatusInputFocus(status: boolean) {
+    this.statusInputFocus = status;
+  }
 
 }
